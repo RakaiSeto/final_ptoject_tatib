7f422de552ccf496ada39101d6939b3b6489ac77
Rahmad Dwi Ferdyan <rahmadferdyan440@gmail.com>
2024-12-05 12:15:11 +0700

    sidebar

---
+++ user/src/View/mahasiswa/page/sidebar.php
@@ -0,0 +1,27 @@
+<!-- Sidebar -->
+<div class="sidebar" id="side_nav">
+      <div class="header-box px-5 pt-3 pb-2 d-flex justify-content-between">
+        <img alt="Logo E-Tatib" src="/public/img/logo-svg" />
+        <button class="btn d-md-none d-block close-btn px-1 py-0 text-dark">
+          <i class="fas fa-stream"></i>
+        </button>
+      </div>
+      <ul class="list-unstyled px-2 py-1">
+        <li class="active">
+          <a href="dashboard.php" class="text-decoration-none px-3 py-3 d-block fw-bold"><i class="fas fa-home"></i>
+            Dashboard</a>
+        </li>
+        <li>
+          <a href="dataPelanggaran.php" class="text-decoration-none px-3 py-3 d-block fw-bold"><i class="fas fa-book"></i>
+            Data Pelanggaran</a>
+        </li>
+        <li>
+          <a href="cetakSurat.php" class="text-decoration-none px-3 py-3 d-block fw-bold"><i class="fas fa-print"></i>
+            Cetak Surat</a>
+        </li>
+        <li>
+          <a href="informasi.php" class="text-decoration-none px-3 py-3 d-block fw-bold"><i class="fas fa-info-circle"></i>
+            Informasi</a>
+        </li>
+      </ul>
+    </div>
