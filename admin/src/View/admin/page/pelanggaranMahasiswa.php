c371766fb982ffeb79909c14dfd3524a3d781f3b
Agung Fradiansyah <143499107+agungfradiansyah12@users.noreply.github.com>
2024-12-22 21:50:17 +0700

    mengubah tampilan detail data pelanggaran di admin

--- admin/src/View/admin/page/pelanggaranMahasiswa.php
+++ admin/src/View/admin/page/pelanggaranMahasiswa.php
@@ -174,75 +174,52 @@
                 <!-- Modal -->
                 <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel"
                     aria-hidden="true" data-bs-backdrop="static" style="background-color: rgba(255, 255, 255, 0.20);">
-                    <div class="modal-dialog modal-lg modal-dialog-centered">
-                        <div class="modal-content" style="background-color: #F5F5F5">
+                    <div class="modal-dialog modal-xl modal-dialog-centered modal-dialog-scrollable">
+                        <div class="modal-content">
                             <div class="modal-header">
                                 <h5 class="modal-title fw-bold" id="exampleModalLabel">Detail Pelanggaran</h5>
                                 <button type="button" class="btn-close" data-bs-dismiss="modal"
                                     aria-label="Close"></button>
                             </div>
                             <div class="modal-body">
-                                <div class="bg-body-tertiary">
-
-                                    <div class="form-group">
-                                        <div class="row mb-3">
-                                            <label for="" class="col-sm-3 col-form-label text-end fw-bold">NIM</label>
-                                            <div class="col-sm-9">
-                                                <input type="text" class="form-control" value="2541987544" readonly>
-                                            </div>
-                                        </div>
-                                        <div class="row mb-3">
-                                            <label for="" class="col-sm-3 col-form-label text-end fw-bold">Nama</label>
-                                            <div class="col-sm-9">
-                                                <input type="text" class="form-control" value="No Name" readonly>
-                                            </div>
-                                        </div>
-                                        <div class="row mb-3">
-                                            <label for="" class="col-sm-3 col-form-label text-end fw-bold">Kelas</label>
-                                            <div class="col-sm-9">
-                                                <input type="text" class="form-control" value="2E" readonly>
-                                            </div>
-                                        </div>
-                                        <div class="row mb-3">
-                                            <label for="" class="col-sm-3 col-form-label text-end fw-bold">Jenis
-                                                Pelanggaran</label>
-                                            <div class="col-sm-9">
-                                                <input type="text" class="form-control" value="Terlambat" readonly>
-                                            </div>
-                                        </div>
-                                        <div class="row mb-3">
-                                            <label for=""
-                                                class="col-sm-3 col-form-label text-end fw-bold">Tingkat</label>
-                                            <div class="col-sm-9">
-                                                <input type="text" class="form-control" value="Ringan" readonly>
-                                            </div>
-                                        </div>
-                                        <div class="row mb-3">
-                                            <label for=""
-                                                class="col-sm-3 col-form-label text-end fw-bold">Tanggal</label>
-                                            <div class="col-sm-9">
-                                                <input type="text" class="form-control" value="28 Februari 2024"
-                                                    readonly>
-                                            </div>
-                                        </div>
-                                        <div class="row mb-3">
-                                            <label for=""
-                                                class="col-sm-3 col-form-label text-end fw-bold">Catatan</label>
-                                            <div class="col-sm-9">
-                                                <input type="text" class="form-control"
-                                                    value="Datang terlambat lebih dari 15 menit" readonly>
-                                            </div>
-                                        </div>
-                                        <div class="row mb-3">
-                                            <label for="" class="col-sm-3 col-form-label text-end fw-bold">Bukti
-                                                Pelanggaran</label>
-                                            <div class="col-sm-9">
-                                                <button class="btn btn-success" data-bs-toggle="modal"
-                                                    data-bs-target="#buktiModal">Lihat Bukti Pelanggaran</button>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
+                                <table class="table table-bordered mb-0">
+                                    <tbody>
+                                        <tr>
+                                            <td class="fw-semibold " style="width: 200px;">NIM Mahasiswa</td>
+                                            <td> <span id="pegawaiNIP"></span></td>
+                                        </tr>
+                                        <tr>
+                                            <td class="fw-semibold">Nama Mahasiswa</td>
+                                            <td> <span id="pegawaiNama"></span></td>
+                                        </tr>
+                                        <tr>
+                                            <td class="fw-semibold">Tanggal Pelanggaran</td>
+                                            <td> <span id="pegawaiRole"></span></td>
+                                        </tr>
+                                        <tr>
+                                            <td class="fw-semibold">Jenis Pelanggaran</td>
+                                            <td> <span id="pegawaiEmail"></span></td>
+                                        </tr>
+                                        <tr>
+                                            <td class="fw-semibold">Tingkat Pelanggaran</td>
+                                            <td> <span id="pegawaiTelp"></span></td>
+                                        </tr>
+                                        <tr>
+                                            <td class="fw-semibold">Dosen Pelapor</td>
+                                            <td> <span id="pegawaiProdi"></span></td>
+                                        </tr>
+                                        <tr>
+                                            <td class="fw-semibold">Keterangan</td>
+                                            <td> <span id="pegawaiJabatan"></span></td>
+                                        </tr>
+                                        <tr>
+                                            <td class="fw-semibold">Bukti Pelanggaran</td>
+                                            <td> <span id="pegawaiBukti"><button class="btn-detail btn-sm"
+                                                        data-bs-toggle="modal" data-bs-target="#buktiModal">Lihat
+                                                        bukti</button></span></td>
+                                        </tr>
+                                    </tbody>
+                                </table>
                             </div>
                         </div>
                     </div>
